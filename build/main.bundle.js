ddf717f2f3d3170534590ef4852d12c7242c086d
tamonwan <ningka_2540@hotmal.com>
2017-08-29 23:23:04 +0700

    add

---
+++ build/main.bundle.js
@@ -0,0 +1,10 @@
+'use strict';
+
+///ESS
+function main() {
+    for (var i = 0; i < 1; i++) {
+        console.log('internal i', i);
+    }
+    console.log('external i', i);
+}
+main();
---
+++ js/index.js
@@ -0,0 +1,8 @@
+///ESS
+function main(){
+    for(var i =0;i<1;i++){
+        console.log('internal i',i);
+    }
+    console.log('external i',i);
+}
+main()
